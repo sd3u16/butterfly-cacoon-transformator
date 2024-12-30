f60cee1796b39f15c2d88076e385e3f118abea51
sd3u16 <s.dimitrov1617@gmail.com>
2024-12-30 23:32:33 +0200

    message

--- notion-clone/src/App.tsx
+++ notion-clone/src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { isCompositeComponent } from 'react-dom/test-utils';
+
+type StageNames = "Caterpillar" | "Cocoon" | "Butterfly";
+
+interface LifeStage{
+  component:string;
+  clicksNeeded: number;
+  nextStage: StageNames | null;
+}
+
+const lifeStages: Record<StageNames, LifeStage> = {
+  Caterpillar: {
+    component: 'Caterpillar',
+    clicksNeeded: 10,
+    nextStage:"Cocoon"
+  },
+  Cocoon: {component: 'Cocoon', clicksNeeded:10, nextStage: "Butterfly"},
+  Butterfly: {component: 'Butterfly', clicksNeeded:0, nextStage: null},
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [stage, setStage] = useState<LifeStage>(lifeStages.Caterpillar)
+  const [clickCount, setClickCount] = useState<number>(0)
+  const [isPressed, setIsPressed] = useState(false)
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+
+  const handleMouseDown = () =>{
+    setIsPressed(true);
+  }
+
+  const handleMouseUp = () => {
+    timeoutRef.current = setTimeout(()=>{
+      setIsPressed(false)
+      timeoutRef.current = null;
+    }, 50)
+  }
+
+  const handleClick = () =>{
+    if(clickCount < stage.clicksNeeded -1){
+      setClickCount(prevCount => prevCount +1)
+    } else if (stage.nextStage){
+      setStage(lifeStages[stage.nextStage]);
+      setClickCount(0)
+    }
+  }
+
+  const StageComponent = stage.component
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container' onClick={handleClick} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown} onMouseLeave={handleMouseUp}>
+      <StageComponent style={{scale: isPressed ? "0.95":"1"}} className="clickable" />
+    </div>
   )
 }
 
